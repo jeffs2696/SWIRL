a2f33df6839bd3a4dd11b735b3f78bf7a37401b1
Jeff Severino <jeffs2696@gmail.com>
2021-06-08 18:40:26 -0400

    cleaned up the main swirl code, in particular the mms variables

--- ObjectOrientedSWIRL/SWIRLCode/Swirl_Modes/SourceFiles/srcF2020/main.f90
+++ ObjectOrientedSWIRL/SWIRLCode/Swirl_Modes/SourceFiles/srcF2020/main.f90
@@ -21,12 +21,9 @@ PROGRAM MAIN
         frequency                ,& !non-dimensional frequency
         hubAdmittance            ,& !Liner Admittance At the Hub
         ductAdmittance           ,&
-        ! axialWavenumber          ,&
-        axialWavenumberAnalytical,&
         ci                       ,&
         gam                      ,&
         gm1                      ,&
-        ! alpha                    ,&
         k_1                      ,&
         k_2                      ,&
         k_3                      ,&
@@ -37,61 +34,19 @@ PROGRAM MAIN
 
     REAL(KIND = REAL64), DIMENSION(:), ALLOCATABLE :: &
         r                   ,&
-        rOut                ,&
-        ! drArray             ,&
         axialMachData       ,&
-        axialMachDataOut    ,&
-        axialMachData_dr_Out,&
         thetaMachData       ,&
         thetaMachDataOut    ,&
-        thetaMachData_dr_Out,&
         totalMachData       ,&
-        SoundSpeedExpected          ,&
-        SoundSpeedOut       ,&
-        SoundSpeed_dr_Out   ,&
-        smachAnalytical     ,&
-        vRPertubation       ,&
-        vThPertubation      ,&
-        vXPertubation       ,&
-        pPertubation        ,&
-        dp_dr               ,&
-        dsmach_dr           ,&
-        drmach_dr           ,&
-        drvel_dr            ,&
-        vRResidual          ,&
-        vThResidual         ,&
-        vXResidual          ,&
-        pResidual           ,&
-        vRSource            ,&
-        vThSource           ,&
-        vXSource            ,&
-        pSource             ,&
-        snd                 ,&
-        ! sndL2Array          ,&
-        sndError            ,&
-        errorMMS
+        SoundSpeedExpected         
 
     REAL(KIND = REAL64) ::  &
         secondOrderSmoother ,& !2nd order smoothing coefficient
         fourthOrderSmoother ,& !4th order smoothing coefficient
-        ! L2res               ,&
-        ! sndL2res            ,&
-        ! errorSum            ,&
-        ! errorSquared        ,&
         boundingConstant    ,&
         dr                  ,&
         hubToTipRatio      ! hub-to-tip ratio
 
-    COMPLEX(KIND = REAL64), DIMENSION(:), ALLOCATABLE :: &
-        ! alphaArray              ,&
-        radialModeData          ,&
-        residualVector          ,&
-        residualVectorAnalytical,&
-        ! L2res_array             ,&
-        S_1                     ,&
-        S_2                     ,&
-        S_3                     ,&
-        S_4
 
     ! INTEGER:: nPts  = 201  ! indended for flow data only and not the grid
 
@@ -101,13 +56,6 @@ PROGRAM MAIN
         rVelMax = 0.00_rDef  ,&
         slope   = 0.0_rDef   ,&
         angom   = 0.00_rDef
-
-    ! CHARACTER(50):: &
-        ! file_name1 , &
-        ! file_name2 , &
-        ! file_name3 !, &
-        ! file_name4 , &
-        ! file_name5
 
     CHARACTER(30):: FORMAT
 
@@ -137,7 +85,6 @@ PROGRAM MAIN
     azimuthalModeNumber       = 2
     hubToTipRatio             = radMin/radMax
     frequency                 =  CMPLX(20.0, 0, rDef)
-    axialWavenumberAnalytical =  0.50_rDef
     hubAdmittance             =  0.40_rDef
     ductAdmittance            =  0.70_rDef
     finiteDiffFlag            =  1
@@ -159,108 +106,32 @@ PROGRAM MAIN
     k_6 = CMPLX(0.0, 0.0, rDef)
     k_7 = CMPLX(0.0, 0.0, rDef)
 
-    WRITE(6,*) ' '
-    WRITE(6,*) '-------------------------------------------------------------------------------------------------'
-    WRITE(6,*) '    INPUT DECK: '
-    WRITE(6,*) '        Azimuthal Mode Number' , azimuthalModeNumber
-    WRITE(6,*) '        Hub to Tip Ratio     ' , hubToTipRatio
-    WRITE(6,*) '        Frequency            ' , frequency
-    WRITE(6,*) '        Hub Liner Admittance ' , hubAdmittance
-    WRITE(6,*) '        Duct Liner Admittance' , ductAdmittance
-    WRITE(6,*) '        Second Order Smoother' , secondOrderSmoother
-    WRITE(6,*) '        Fourth Order Smoother' , fourthOrderSmoother
-    IF (finiteDiffFlag.eq.0) THEN
-        WRITE(6,*) '        Finite Difference Flag' , finiteDiffFlag
-        WRITE(6,*) '        --> Spectral Differencing is used for the radial derivatives'
-    ELSEIF (finiteDiffFlag.eq.1) THEN
-        WRITE(6,*) '        Finite Difference Flag' , finiteDiffFlag
-        WRITE(6,*) '        --> Second Order Differencing is used for the radial derivatives'
-    ELSEIF (finiteDiffFlag.eq.2) THEN
-        WRITE(6,*) '        Finite Difference Flag' , finiteDiffFlag
-        WRITE(6,*) '       --> Fourth Order Differencing is used for the radial derivatives'
-    ENDIF
-
-    WRITE(6,*) '-------------------------------------------------------------------------------------------------'
-    WRITE(6,*) ' '
     ! Starting Grid DO LOOP
 
     First_fac  = 1
-    Last_fac   = 5
+    Last_fac   = 1
 
     facCount = 0 ! initializer for fac count
-    ! file_name2 = 'L2vsDeltar.dat'
-    ! file_name3 = 'SndSpeedRateOfConvergence.dat'
-    ! OPEN(345, FILE = TRIM(file_name2))
-    ! OPEN(344, FILE = TRIM(file_name3))
-
 
     WRITE(6, *) '       Number of Grid Study Iterations: ' , Last_fac-First_fac + 1
 
     DO fac = First_fac, Last_fac
+
         facCount = facCount + 1
         numberOfGridPoints   = 1+(2**fac)
 
-
-        ! write integer into a string
         WRITE(file_id, '(i0)') numberOfGridPoints
-
-        ! Construct the filename:
-
-        ! file_name1 = 'SoundSpeedMMS' // trim(adjustl(file_id)) // '.dat'
-        ! file_name4 = 'FlowDataInput' // trim(adjustl(file_id)) // '.dat'
-
-        ! file_name5 = 'FlowDataOutput' // trim(adjustl(file_id)) // '.dat'
-
-        !    OPEN(145, FILE = TRIM(file_name1))
 
         WRITE(6, *) '       # Grid Points:                   ',  numberOfGridPoints
 
-        ! WRITE(6, *) '       ALLOCATING SwirlClassObj Arrays ...'
         ALLOCATE(&
-             S_1(numberOfGridPoints)                         ,&
-            S_2(numberOfGridPoints)                         ,&
-            S_3(numberOfGridPoints)                         ,&
-            S_4(numberOfGridPoints)                         ,&
-            radialModeData(numberOfGridPoints*4)            ,&
-            residualVector(numberOfGridPoints*4)            ,&
-            residualVectorAnalytical(numberOfGridPoints*4)  ,&
-            errorMMS(numberOfGridPoints*4)                  ,&
-            sndError(numberOfGridPoints)                    ,&
             r(numberOfGridPoints)                                         ,&
-            rOut(numberOfGridPoints)                                         ,&
-            snd(numberOfGridPoints)                                       ,&
             thetaMachData(numberOfGridPoints)                             ,&
             thetaMachDataOut(numberOfGridPoints)                             ,&
-            thetaMachData_dr_Out(numberOfGridPoints)                             ,&
-            smachAnalytical(numberOfGridPoints)                           ,&
             axialMachData(numberOfGridPoints)                             ,&
-            axialMachDataOut(numberOfGridPoints)                             ,&
-            axialMachData_dr_Out(numberOfGridPoints)                             ,&
             totalMachData(numberOfGridPoints)                             ,&
             SoundSpeedExpected(numberOfGridPoints)                                ,&
-            SoundSpeedOut(numberOfGridPoints)                             ,&
-            SoundSpeed_dr_Out(numberOfGridPoints)                             ,&
-            vRPertubation(numberOfGridPoints)                             ,&
-            vThPertubation(numberOfGridPoints)                            ,&
-            vXPertubation(numberOfGridPoints)                             ,&
-            pPertubation(numberOfGridPoints)                              ,&
-            vRResidual(numberOfGridPoints)                                ,&
-            vThResidual(numberOfGridPoints)                               ,&
-            vXResidual(numberOfGridPoints)                                ,&
-            pResidual(numberOfGridPoints)                                 ,&
-            vRSource(numberOfGridPoints)                                  ,&
-            vThSource(numberOfGridPoints)                                 ,&
-            vXSource(numberOfGridPoints)                                  ,&
-            pSource(numberOfGridPoints)                                   ,&
-            dp_dr(numberOfGridPoints)                                     ,&
-            dsmach_dr(numberOfGridPoints)                       ,&
-            drmach_dr(numberOfGridPoints)                       ,&
-            drvel_dr(numberOfGridPoints) )
-
-
-        ! WRITE(6, *) '       DONE ALLOCATING SwirlClassObj Arrays ...'
-
-        WRITE(6,*) '        Defining Radial Domain ...'
+            )
 
         dr = (radMax-radMin)/REAL(numberOfGridPoints-1, rDef)
 
@@ -268,9 +139,6 @@ PROGRAM MAIN
             r(i) = (radMin+REAL(i-1, rDef)*dr)/radMax
         END DO
 
-        WRITE(6,*) '        Defining Flow Domain ...'
-
-        ! Headers for Flow Domain Data'
 
         WRITE(6,*) ' '
         WRITE(6,'(a)') 'Table 1: Input Flow Data'
@@ -284,10 +152,8 @@ PROGRAM MAIN
                 (boundingConstant)*&
                 EXP(REAL(k_2, rDef)*(r(i)-1.0_rDef))
 
-
-
             thetaMachData(i) = &
-            SQRT(2.0_rDef)*&
+                SQRT(2.0_rDef)*&
                 SQRT(-(REAL(k_3,rDef)*r(i)*&
                 SIN(REAL(k_3,rDef)*(r(i)-1.0_rDef)))/&
                 (REAL(gm1,rDef)*COS(REAL(k_3,rDef)*(r(i)-1.0_rDef))))
@@ -302,7 +168,6 @@ PROGRAM MAIN
                 ((axialMachData(i)**2.0_rDef+&
                 thetaMachData(i)**2.0_rDef)**0.5_rDef)
 
-
             IF(totalMachData(i) > 1.0_rDef) THEN
                 WRITE(6, *) i, 'ERROR: Total mach is greater than one'
                 STOP
@@ -312,24 +177,8 @@ PROGRAM MAIN
 
             ENDIF
 
-!            vRPertubation(i)  = EXP(REAL(k_4, rDef)*r(i))
-!            vThPertubation(i) = EXP(REAL(k_5, rDef)*r(i))
-!            vXPertubation(i)  = EXP(REAL(k_6, rDef)*r(i))
-!            pPertubation      = EXP(REAL(k_7, rDef)*r(i))
-!            dp_dr             = REAL(k_7, rDef)*EXP(REAL(k_7)*r(i))
-!            dsmach_dr(i)      = REAL(k_3, rDef)*EXP(REAL(k_3)*r(i))
-!            drmach_dr(i)      = REAL(k_2, rDef)*EXP(REAL(k_2)*r(i))
-!            drvel_dr (i)      = REAL(k_4, rDef)*EXP(REAL(k_4)*r(i))
-
         ENDDO
 
-!        OPEN(12, FILE=file_name4)
-!        DO i = 1,numberOfGridPoints
-!            WRITE(12,*) r(i) , axialMachData(i) , thetaMachData(i), SoundSpeedExpected(i)
-!        ENDDO
-!        CLOSE(12)
-!
-        !------------------------------------------------------------------------------
         CALL CreateObject(&
             object        = swirlClassObj  ,&
             azimuthalMode = azimuthalModeNumber  ,&
@@ -337,7 +186,6 @@ PROGRAM MAIN
             sig           = hubToTipRatio        ,&
             AxialMachData = axialMachData        ,&
             ThetaMachData = thetaMachData        ,&
-            ! SoundSpeed    = SoundSpeedOut        ,&
             ak            = frequency            ,&
             etah          = hubAdmittance        ,&
             etad          = ductAdmittance       ,&
@@ -347,46 +195,15 @@ PROGRAM MAIN
 
 
         CALL DestroyObject(object = swirlClassObj)
+
         DEALLOCATE(&
-            S_1                       ,&
-            S_2                       ,&
-            S_3                       ,&
-            S_4                       ,&
-            radialModeData            ,&
-            residualVector            ,&
-            residualVectorAnalytical  ,&
-            errorMMS                  ,&
-            sndError                  ,&
             r                         ,&
-            rOut                         ,&
-            snd                       ,&
-            vRPertubation             ,&
-            vThPertubation            ,&
-            vXPertubation             ,&
-            pPertubation              ,&
-            vRSource                  ,&
-            vThSource                 ,&
-            vXSource                  ,&
-            pSource                   ,&
-            vRResidual                ,&
-            vThResidual               ,&
-            vXResidual                ,&
-            pResidual                 ,&
             thetaMachData             ,&
             thetaMachDataOut             ,&
-            thetaMachData_dr_Out             ,&
-            smachAnalytical           ,&
             axialMachData             ,&
-            axialMachDataOut             ,&
-            axialMachData_dr_Out             ,&
             totalMachData             ,&
             SoundSpeedExpected                ,&
-            SoundSpeedOut             ,&
-            SoundSpeed_dr_Out             ,&
-            dp_dr                     ,&
-            drmach_dr                 ,&
-            dsmach_dr                 ,&
-            drvel_dr)
+            )
 
     END DO
 END PROGRAM MAIN
